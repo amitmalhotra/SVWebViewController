44d00b22819e7ed4485a1fe8c4e8f45e12484e15
Amit Malhotra <amit.malhotra@trackvia.com>
2013-11-20 14:26:46 -0700

    Making the Menu Buttons public so you can control to show or hide them.

--- SVWebViewController/SVWebViewController.m
+++ SVWebViewController/SVWebViewController.m
@@ -12,14 +12,6 @@
 
 @interface SVWebViewController () <UIWebViewDelegate>
 
-@property (nonatomic, strong) UIBarButtonItem *backBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *forwardBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *refreshBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *stopBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *actionBarButtonItem;
-
-@property (nonatomic, strong) UIWebView *webView;
-@property (nonatomic, strong) NSURL *URL;
 
 - (id)initWithAddress:(NSString*)urlString;
 - (id)initWithURL:(NSURL*)URL;
